f716ecdcdf8f38f2cc4798eda7b1e2a8ca8c64a5
Perin <joaovperin@live.com>
2018-08-30 23:46:28 -0300

    Added AZURE integration, now it can be deployed

--- src/LunchChatbot.js
+++ src/LunchChatbot.js
@@ -7,11 +7,11 @@ const UniversalBot = require('./bot/universal-bot.js');
 
 // Exports the app
 module.exports = (() => {
-    
+
     // Check if running under a development profile
     const isDevelopment = process.env.LUNCH_DEV ? true : false;
     console.log("Development: " + isDevelopment);
-    
+
     if (isDevelopment) {
         const connector = new builder.ConsoleConnector().listen();
         var bot = UniversalBot.createBot(connector);
@@ -19,21 +19,23 @@ module.exports = (() => {
         // Setup Restify Server
         const server = restify.createServer();
         server.listen(process.env.port || process.env.PORT || 3978, function () {
-            console.log('%s listening to %s', server.name, server.url); 
+            console.log('%s listening to %s', server.name, server.url);
         });
         // Create chat connector for communicating with the Bot Framework Service
         var connector = new builder.ChatConnector({
             appId: process.env.MicrosoftAppId,
-            appPassword: process.env.MicrosoftAppPassword
+            appPassword: process.env.MicrosoftAppPassword,
+            openIdMetadata: process.env.BotOpenIdMetadata
         });
-        
+
         // Listen for messages from users 
         server.post('/api/messages', connector.listen());
         var bot = UniversalBot.createBot(connector);
+
     }
-    
+
     // External functions to export
     return {
-        run: ()=> console.log('Running')
+        run: () => console.log('Running')
     }
 })();
--- src/bot/universal-bot.js
+++ src/bot/universal-bot.js
@@ -1,16 +1,34 @@
 // Requires the dependencies
 const builder = require('botbuilder');
+const botbuilder_azure = require("botbuilder-azure");
 
 // Exports the app
 module.exports = (() => {
-    
+
+    function createBot(connector) {
+        const bot = new builder.UniversalBot(connector);
+        // Setup storage
+        var tableName = 'botdata';
+        var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+        var tableStorage = new botbuilder_azure.AzureBotStorage({
+            gzipData: false
+        }, azureTableClient);
+
+        // Set storage and listen to root
+        var st = bot.get('storage');
+        bot.set('storage', tableStorage); // Comment this to make it run locally
+        bot.dialog('/', responseService);
+
+        return bot;
+    }
+
     // Service function to answer
-    function responseService(session){
+    function responseService(session) {
         session.send("-> You said: %s <-", session.message.text);
     }
-    
+
     // External functions to export
     return {
-        createBot: (connector) => new builder.UniversalBot(connector, responseService)
+        createBot: createBot
     }
 })();
